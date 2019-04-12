8c2c98dcb2aab1071fe5d54094790d074eecd51c
Susan Sons <susan@magic>
2019-04-12 15:29:54 -0400

    Fixed rendering on level 1; temporary theme
    added YAML header to level 1, set base as theme temporarily for debugging

--- conf.py
+++ conf.py
@@ -143,8 +143,8 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "trustedguide"
-#THEME = "blue"
+#THEME = "trustedguide"
+THEME = "base"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
--- pages/ComingSoon.md
+++
@@ -1,5 +0,0 @@
-<!--
-.. pretty_url: False
-.. link: /ComingSoon.html
--->
-<p style="text-align: center;"> Coming Soon!</p>
